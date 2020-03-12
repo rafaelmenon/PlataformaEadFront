eb8fee6583e5cc367dbab1cdc42c2bd14cbc4ed6
Rafael Menon <rafael13rodrigo@gmail.com>
2020-03-12 11:54:18 -0300

    Base da aplicação.

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Switch } from "react-router-dom";
+import Route from "./Route";
+import Login from "../pages/login";
+import Dashboard from "../pages/dashboard";
+
+const Routes = () => {
+  return (
+    <Switch>
+      <Route path="/login" component={Login} />
+      <Route path="/" exact={true} component={Dashboard} isProtect />
+    </Switch>
+  );
+};
+
+export default Routes;
---
+++ src/store/modules/user/reducer.js
@@ -0,0 +1,33 @@
+import produce from "immer";
+
+const INITIAL_STATE = {
+  profile: {
+    id: "",
+    name: "",
+    email: ""
+  }
+};
+
+export default function user(state = INITIAL_STATE, action) {
+  const { type, payload } = action;
+
+  return produce(state, draft => {
+    switch (type) {
+      case "@auth/SIGN_IN_SUCCESS": {
+        draft.profile = payload.user;
+        break;
+      }
+
+      case "@auth/SIGN_OUT": {
+        draft.profile = {
+          id: "",
+          name: "",
+          email: ""
+        };
+        break;
+      }
+
+      default:
+    }
+  });
+}
